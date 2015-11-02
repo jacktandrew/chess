a27460670169ca051f6342bc57c1eddc7965bf9a
Jacandre <jacandre@amazon.com>
2015-11-02 14:05:21 -0800

    add ai.js to partially automate testing

---
+++ ai.js
@@ -0,0 +1,30 @@
+window.chess.ai = {
+	counter: 0,
+	white: [
+		['e2', 'e4'],
+		['f1', 'c4'],
+		['d1', 'h5'],
+	],
+	black: [
+		['e7', 'e5'],
+		['b8', 'c6'],
+		['g8', 'h6'],
+		['a7', 'a6'],
+	],
+	play: function() {
+		this.white.forEach(function(move, i) {
+			var white = chess.ai.getSquares('white', i),
+				black = chess.ai.getSquares('black', i);
+			chess.ai.move(white);
+			chess.ai.move(black);
+		});
+	},
+	getSquares: function(team, i) {
+		return this[team][i].map(function(coord) {
+			return chess.board[coord];
+		});
+	},
+	move: function(coords) {
+		chess.game.movePiece(coords[0], coords[1]);
+	}
+};
