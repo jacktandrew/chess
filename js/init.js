c08f389dd8ba6943594f93fc63bb15f67639a2dd
Jack Andrew <jacktandrew@gmail.com>
2015-11-04 18:56:49 -0800

    move init into own file

---
+++ js/init.js
@@ -0,0 +1,11 @@
+var boardEl = document.querySelector('.board'),
+  board = chess.board = new chess.Board();
+
+chess.team = {};
+
+var white = chess.team.white = new chess.Pieces('white'),
+  black = chess.team.black = new chess.Pieces('black');
+
+chess.setup.init(board, boardEl, white, black);
+
+chess.game = new chess.Game(board);
